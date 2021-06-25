10256f08b8b6a7a60631cbed4a3323e0612ce55a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-06-25 17:53:45 +0000

    FEA: adds repr to nodes

--- pydecisiontree/nodes.py
+++ pydecisiontree/nodes.py
@@ -18,21 +18,80 @@ class Nodes:
     def __getitem__(self, name: str) -> dict:
         return self.data[name]
 
-    def terminal(self, name: str, expr: Any) -> None:
-        self.data[name] = {
-            "type": "TERMINAL",
-            "expr": expr,
-        }
-
     def chance(self, name: str, branches: List[tuple]) -> None:
+        """Adds a chance node."""
         self.data[name] = {
             "type": "CHANCE",
             "branches": branches,
         }
 
     def decision(self, name: str, branches: List[tuple], max_: bool = False) -> None:
+        """Adds a decision node"""
         self.data[name] = {
             "type": "DECISION",
             "branches": branches,
             "max_": max_,
         }
+
+    def terminal(self, name: str, expr: Any) -> None:
+        """Adds a terminal node"""
+        self.data[name] = {
+            "type": "TERMINAL",
+            "expr": expr,
+        }
+
+    def __repr__(self):
+        def repr_decision(name, node):
+
+            text = []
+            text.append("    Type: " + node.get("type"))
+            text[-1] += (
+                " - Maximum Payoff" if node.get("max_") is True else " - Minimum Payoff"
+            )
+            text.append("    Name: " + name)
+            text.append("    Branches:")
+            text.append("                         Value  Next Node")
+            for (outcome, next_node) in node.get("branches"):
+                text.append(
+                    "                  {:12.3f}  {:s}".format(outcome, next_node)
+                )
+            text.append("")
+            return text
+
+        def repr_chance(name, node):
+            text = []
+            text.append("    Type: " + node.get("type"))
+            text.append("    Name: " + name)
+            text.append("    Branches:")
+            text.append("          Chance         Value  Next Node")
+            for (prob, outcome, next_node) in node.get("branches"):
+                text.append(
+                    "           {:5.2f}  {:12.3f}  {:s}".format(
+                        prob, outcome, next_node
+                    )
+                )
+            text.append("")
+            return text
+
+        def repr_terminal(name, node):
+            text = []
+            text.append("    Type: " + node.get("type"))
+            text.append("    Name: " + name)
+            if node.get("expr") is None:
+                text.append("    Expr: (cumulative)")
+            else:
+                text.append("    Expr: (User fn)")
+            text.append("")
+            return text
+
+        text = []
+        for i_node, (name, node) in enumerate(self.data.items()):
+            text.append("Node {}".format(i_node))
+            if node.get("type") == "DECISION":
+                text += repr_decision(name=name, node=node)
+            if node.get("type") == "CHANCE":
+                text += repr_chance(name=name, node=node)
+            if node.get("type") == "TERMINAL":
+                text += repr_terminal(name=name, node=node)
+
+        return "\n".join(text)
--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -3,9 +3,15 @@
 
 from pydecisiontree.nodes import Nodes
 
+#
+#
+# T e r m i n a l    n o d e s
+#
+#
+
 
 def test_adds_terminal_node():
-
+    """terminal node"""
     name = "node1"
     expr = lambda x: x + 1
 
@@ -16,8 +22,24 @@ def test_adds_terminal_node():
     assert nodes[name].get("expr") == expr
 
 
+def test_repr_terminal_node():
+    """Checks repr function"""
+    name = "node1"
+    expr = lambda x: x + 1
+
+    nodes = Nodes()
+    nodes.terminal(name=name, expr=expr)
+
+    assert (
+        nodes.__repr__() == "Node 0\n"
+        "    Type: TERMINAL\n"
+        "    Name: node1\n"
+        "    Expr: (User fn)\n"
+    )
+
+
 def test_adds_chance_node():
-    #
+    """chance node"""
     name = "chance_node"
     branches = [
         (20.0, 100, "next_node"),
@@ -32,8 +54,34 @@ def test_adds_chance_node():
     assert nodes[name].get("branches") == branches
 
 
-def test_adds_decision_node():
+def test_repr_chance_node():
+    """Checks repr"""
+    name = "chance_node"
+    branches = [
+        (20.0, 100, "next_node"),
+        (30.0, 200, "next_node"),
+        (50.0, 300, "next_node"),
+    ]
 
+    nodes = Nodes()
+    nodes.chance(name=name, branches=branches)
+
+    print(nodes.__repr__())
+
+    assert (
+        nodes.__repr__() == "Node 0\n"
+        "    Type: CHANCE\n"
+        "    Name: chance_node\n"
+        "    Branches:\n"
+        "          Chance         Value  Next Node\n"
+        "           20.00       100.000  next_node\n"
+        "           30.00       200.000  next_node\n"
+        "           50.00       300.000  next_node\n"
+    )
+
+
+def test_adds_decision_node():
+    """decison node"""
     name = "decision_node"
     branches = [
         (20.0, "next_node"),

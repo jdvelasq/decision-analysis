aa9d79f5f36598830fd81cf8b51aeba51e14d89d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-07-02 04:59:43 +0000

    feat: dependent probabilities

--- dmak/decisiontree.py
+++ dmak/decisiontree.py
@@ -293,6 +293,55 @@ class DecisionTree:
 
         return "\n".join(lines)
 
+    # -------------------------------------------------------------------------
+    #
+    #
+    #  D E P E N D E N T    P R O B A B I L I T I E S
+    #
+    #
+    def set_dependent_probabilities(
+        self, variable: str, depends_on: Any, probabilities: dict
+    ) -> None:
+        """Set probability values in a chance node dependent on previous nodes.
+
+        :param variable:
+
+        :param depends_on:
+
+        :param probabilities:
+
+
+        """
+
+        def dispatch(idx: int, args: dict) -> None:
+
+            args = args.copy()
+
+            if "tag_name" in self._nodes[idx].keys():
+                tag_name = self._nodes[idx]["tag_name"]
+                tag_value = self._nodes[idx]["tag_value"]
+                args = {**args, **{tag_name: tag_value}}
+
+            name = self._nodes[idx].get("name")
+            if name == variable:
+
+                if isinstance(depends_on, list):
+                    key = tuple(args[term] for term in depends_on)
+                else:
+                    key = args[depends_on]
+
+                probs = probabilities[key]
+
+                for i_successor, successor in enumerate(self._nodes[idx]["successors"]):
+
+                    self._nodes[successor]["tag_prob"] = probs[i_successor]
+
+            if "successors" in self._nodes[idx].keys():
+                for successor in self._nodes[idx]["successors"]:
+                    dispatch(idx=successor, args=args)
+
+        dispatch(idx=0, args={})
+
     ##
     ##
     ##
@@ -347,46 +396,6 @@ class DecisionTree:
                         set_fn_args(idx=successor, args=args)
 
         set_fn_args(idx=0, args={})
-
-    # -------------------------------------------------------------------------
-    #
-    #
-    #  D E P E N D E N T    P R O B A B I L I T I E S
-    #
-    #
-    def set_dependent_probabilities(
-        self, variable: str, depends_on: Any, probabilities: dict
-    ) -> None:
-        """Set probability values in a chance node dependent on previous nodes"""
-
-        def dispatch(idx: int, args: dict) -> None:
-
-            args = args.copy()
-
-            if "tag_name" in self._nodes[idx].keys():
-                tag_name = self._nodes[idx]["tag_name"]
-                tag_value = self._nodes[idx]["tag_value"]
-                args = {**args, **{tag_name: tag_value}}
-
-            name = self._nodes[idx].get("name")
-            if name == variable:
-
-                if isinstance(depends_on, list):
-                    key = tuple(args[term] for term in depends_on)
-                else:
-                    key = args[depends_on]
-
-                probs = probabilities[key]
-
-                for i_successor, successor in enumerate(self._nodes[idx]["successors"]):
-
-                    self._nodes[successor]["tag_prob"] = probs[i_successor]
-
-            if "successors" in self._nodes[idx].keys():
-                for successor in self._nodes[idx]["successors"]:
-                    dispatch(idx=successor, args=args)
-
-        dispatch(idx=0, args={})
 
     # -------------------------------------------------------------------------
     #
--- tests/test_stguide.py
+++ tests/test_stguide.py
@@ -62,47 +62,129 @@ def test_tree_creation(capsys):
     matcher.fnmatch_lines(captured_text, consecutive=True)
 
 
-#
-# SuperTree user guide simple bid example
-#
+def test_dependent_probabilities(capsys):
+    """Example Fig. 7.2"""
 
-# def test_tree_repr():
+    expected_text = dedent(
+        """
+        STRUCTURE    NAMES    OUTCOMES     PROBABILIES
+        ---------------------------------------------------
+        0D1 14       bid      500 700
+        1C2 6 10     compbid  400 600 800  .350 .500 .150
+        2C3 4 5      cost     200 400 600  .400 .400 .200
+        3T           profit
+        4T           profit
+        5T           profit
+        6C7 8 9      cost     200 400 600  .250 .500 .250
+        7T           profit
+        8T           profit
+        9T           profit
+        10C11 12 13  cost     200 400 600  .100 .450 .450
+        11T          profit
+        12T          profit
+        13T          profit
+        14C15 19 23  compbid  400 600 800  .350 .500 .150
+        15C16 17 18  cost     200 400 600  .400 .400 .200
+        16T          profit
+        17T          profit
+        18T          profit
+        19C20 21 22  cost     200 400 600  .250 .500 .250
+        20T          profit
+        21T          profit
+        22T          profit
+        23C24 25 26  cost     200 400 600  .100 .450 .450
+        24T          profit
+        25T          profit
+        26T          profit
+        """
+    )
+
+    nodes = stguide_bid()
+    tree = DecisionTree(variables=nodes, initial_variable="bid")
+
+    ## Probabilities for COST depends on COMPBID
+    tree.set_dependent_probabilities(
+        variable="cost",
+        depends_on="compbid",
+        probabilities={
+            400: [0.4, 0.4, 0.2],
+            600: [0.25, 0.50, 0.25],
+            800: [0.1, 0.45, 0.45],
+        },
+    )
+
+    print(tree)
+
+    #
+    # Test
+    #
+    captured_text = capsys.readouterr().out.splitlines()
+    captured_text = captured_text[1:]
+    captured_text = [text.rstrip() for text in captured_text]
+    matcher = LineMatcher(expected_text.splitlines()[1:])
+    matcher.fnmatch_lines(captured_text, consecutive=True)
 
 
-# >>> from dmak.examples import stguide_bid
-# >>> from dmak.decisiontree import DecisionTree
-# >>> nodes = stguide_bid()
-# >>> tree = DecisionTree(
-# ...     variables=nodes,
-# ...     initial_variable="bid"
-# ... )
-# >>> tree
-# STRUCTURE    NAMES    OUTCOMES     PROBABILIES
-# <BLANKLINE>
-# 0D1 14       bid      500 700
-# 1C2 6 10     compbid  400 600 800  .350 .500 .150
-# 2C3 4 5      cost     200 400 600  .250 .500 .250
-# 3T           profit
-# 4T           profit
-# 5T           profit
-# 6C7 8 9      cost     200 400 600  .250 .500 .250
-# 7T           profit
-# 8T           profit
-# 9T           profit
-# 10C11 12 13  cost     200 400 600  .250 .500 .250
-# 11T          profit
-# 12T          profit
-# 13T          profit
-# 14C15 19 23  compbid  400 600 800  .350 .500 .150
-# 15C16 17 18  cost     200 400 600  .250 .500 .250
-# 16T          profit
-# 17T          profit
-# 18T          profit
-# 19C20 21 22  cost     200 400 600  .250 .500 .250
-# 20T          profit
-# 21T          profit
-# 22T          profit
-# 23C24 25 26  cost     200 400 600  .250 .500 .250
-# 24T          profit
-# 25T          profit
-# 26T          profit
+def test_dependent_outcomes(capsys):
+    """Example Fig. 7.2"""
+
+    expected_text = dedent(
+        """
+        STRUCTURE    NAMES    OUTCOMES     PROBABILIES
+        ---------------------------------------------------
+        0D1 14       bid      500 700
+        1C2 6 10     compbid  400 600 800  .350 .500 .150
+        2C3 4 5      cost     170 350 550  .250 .500 .250
+        3T           profit
+        4T           profit
+        5T           profit
+        6C7 8 9      cost     200 400 600  .250 .500 .250
+        7T           profit
+        8T           profit
+        9T           profit
+        10C11 12 13  cost     280 450 650  .250 .500 .250
+        11T          profit
+        12T          profit
+        13T          profit
+        14C15 19 23  compbid  400 600 800  .350 .500 .150
+        15C16 17 18  cost     190 380 570  .250 .500 .250
+        16T          profit
+        17T          profit
+        18T          profit
+        19C20 21 22  cost     220 420 610  .250 .500 .250
+        20T          profit
+        21T          profit
+        22T          profit
+        23C24 25 26  cost     300 480 680  .250 .500 .250
+        24T          profit
+        25T          profit
+        26T          profit
+        """
+    )
+
+    nodes = stguide_bid()
+    tree = DecisionTree(variables=nodes, initial_variable="bid")
+
+    ## Probabilities for COST depends on COMPBID, BID
+    tree.set_dependent_outcomes(
+        variable="cost",
+        depends_on=("compbid", "bid"),
+        outcomes={
+            (400, 500): [170, 350, 550],
+            (400, 700): [190, 380, 570],
+            (600, 500): [200, 400, 600],
+            (600, 700): [220, 420, 610],
+            (800, 500): [280, 450, 650],
+            (800, 700): [300, 480, 680],
+        },
+    )
+    print(tree)
+
+    #
+    # Test
+    #
+    captured_text = capsys.readouterr().out.splitlines()
+    captured_text = captured_text[1:]
+    captured_text = [text.rstrip() for text in captured_text]
+    matcher = LineMatcher(expected_text.splitlines()[1:])
+    matcher.fnmatch_lines(captured_text, consecutive=True)

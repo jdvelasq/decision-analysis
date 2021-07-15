134b564b35f73ca771b480c0c0c5bdeb4d6d823f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-07-15 12:45:55 +0000

    fix: risk sensitivity test

--- tests/test_risk_tolerance.py
+++ tests/test_risk_tolerance.py
@@ -1,7 +1,24 @@
+"""
+Risk sensitivity tests
+
+"""
+
 from smart_choice.decisiontree import DecisionTree
 from smart_choice.examples import stguide
+from smart_choice.risk_sensitivity import RiskSensitivity
 
 from tests.capsys import check_capsys
+
+
+# def _test_fig_7_19(capsys):
+#     """Fig. 7.19 --- Risk Tolerance"""
+
+#     nodes = stguide()
+#     tree = DecisionTree(nodes=nodes)
+#     tree.evaluate()
+#     tree.rollback()
+#     tree.risk_sensitivity(utility_fn="exp", risk_tolerance=75)
+#     check_capsys("./tests/files/stguide_fig_7_19.txt", capsys)
 
 
 def test_fig_7_19(capsys):
@@ -11,5 +28,6 @@ def test_fig_7_19(capsys):
     tree = DecisionTree(nodes=nodes)
     tree.evaluate()
     tree.rollback()
-    tree.risk_sensitivity(utility_fn="exp", risk_tolerance=75)
+    risk_sensitivity = RiskSensitivity(tree, utility_fn="exp", risk_tolerance=75)
+    print(risk_sensitivity)
     check_capsys("./tests/files/stguide_fig_7_19.txt", capsys)

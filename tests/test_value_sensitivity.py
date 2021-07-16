ae7cebb42c992c0e5844f7f2c8de61d910c68d06
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-07-16 03:22:08 +0000

    fix: value sensitivity

--- tests/test_value_sensitivity.py
+++ tests/test_value_sensitivity.py
@@ -4,6 +4,7 @@ Risk profile
 """
 from smart_choice.decisiontree import DecisionTree
 from smart_choice.examples import oil_tree_example
+from smart_choice.value_sensitivity import ValueSensitivity
 
 from tests.capsys import check_capsys
 
@@ -12,11 +13,14 @@ def test_oilexample_pag_34a(capsys):
     """Sensitivity"""
     nodes = oil_tree_example()
     tree = DecisionTree(nodes=nodes)
-    print(
-        tree.value_sensitivity(
-            name="oil_found", branch="large-well", values=(2500, 5000)
-        )
+    sensitivity = ValueSensitivity(
+        decisiontree=tree,
+        varname="oil_found",
+        branch_name="large-well",
+        values=(2500, 5000),
     )
+
+    print(sensitivity)
     check_capsys("./tests/files/oilexample_pag_34a.txt", capsys)
 
 
@@ -24,7 +28,11 @@ def test_oilexample_pag_34b(capsys):
     """Sensitivity"""
     nodes = oil_tree_example()
     tree = DecisionTree(nodes=nodes)
-    print(
-        tree.value_sensitivity(name="drill_decision", branch="drill", values=(450, 750))
+    sensitivity = ValueSensitivity(
+        decisiontree=tree,
+        varname="drill_decision",
+        branch_name="drill",
+        values=(450, 750),
     )
+    print(sensitivity)
     check_capsys("./tests/files/oilexample_pag_34b.txt", capsys)
